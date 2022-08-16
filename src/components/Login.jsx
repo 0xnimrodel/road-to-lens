4c48fddac63b8a2b752d1ce0c37cdca1c7870513
zepgursoy <34766972+zepgursoy@users.noreply.github.com>
2022-08-16 12:42:51 +0300

    changing ui

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { useAccount, useSignMessage } from 'wagmi';
-import { Lens } from 'lens-protocol';
+import React from 'react'
+import { useAccount, useSignMessage } from 'wagmi'
+import { Lens } from 'lens-protocol'
 export default function Login() {
-  const { address } = useAccount();
+  const { address } = useAccount()
   const { data, error, isLoading, signMessage } = useSignMessage({
     onSuccess(data, variables) {
       // Verify the signature
-      VerifySignature(data);
+      VerifySignature(data)
     },
-  });
+  })
 
   const authenticate = async () => {
     // Getting the challenge from the server
-    const data = await Lens.getChallenge(address);
-    let message = data.data.challenge.text;
+    const data = await Lens.getChallenge(address)
+    let message = data.data.challenge.text
     // Signing the challenge with the wallet
-    signMessage({ message });
-  };
+    signMessage({ message })
+  }
 
   const VerifySignature = async (sign) => {
     // Sending the signature to the server to verify
-    const response = await Lens.Authenticate(address, sign);
-    console.log(response);
+    const response = await Lens.Authenticate(address, sign)
+    console.log(response)
 
     // {
     //  data: {
@@ -30,14 +30,17 @@ export default function Login() {
     //    refreshToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjB4YjE5QzI4OTBjZjk0N0FEM2YwYjdkN0U1QTlmZkJjZTM2ZDNmOWJkMiIsInJvbGUiOiJyZWZyZXNoIiwiaWF0IjoxNjQ1MTA0MjMxLCJleHAiOjE2NDUxOTA2MzF9.2Tdts-dLVWgTLXmah8cfzNx7sGLFtMBY7Z9VXcn2ZpE"
     //   }
     // }
-  };
+  }
 
   return (
     <div>
-      <button className='rounded-lg px-5 py-2 dark:bg-lime-500 dark:text-lime-900 font-semibold dark:hover:bg-lime-600' onClick={authenticate}>Login</button>
+      <button className='rounded-lg px-5 py-2 dark:bg-lime-500 dark:text-lime-900 font-semibold dark:hover:bg-lime-600'>
+        Login
+      </button>
     </div>
-  );
+  )
 }
 
-
-{/* <button    onClick={() => disconnect()}>{address.substring(0, 6) + '...' + address.substring(address.length - 6, address.length)}</button> */}
+{
+  /* <button    onClick={() => disconnect()}>{address.substring(0, 6) + '...' + address.substring(address.length - 6, address.length)}</button> */
+}
